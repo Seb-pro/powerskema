88a168561f41c4b9564f02e60cd71f5088dcc9de
Sebastian <58257051+Seb-pro@users.noreply.github.com>
2023-10-24 13:37:43 +0200

    added a basic calendar, localized it to danish (#1)
    * added a basic calendar, localized it to danish
    
    * Fixed header
    
    * added larger threshold for swiper
    
    * Cleaned up the code, added global css to calendar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CalendarMode } from 'ionic7-calendar/calendar.interface';
+import { CalendarComponent } from 'ionic7-calendar';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  calendar = {
+    mode: 'month' as CalendarMode,
+    currentDate: new Date(),
+    formatHourColumn: 'H:MM',
+    formatWeekTitle: `MMM 'uge' w`,
+  };
+  options = {
+    spaceBetween: 10,
+    threshold: 50,
+  };
+  viewTitle: string = '';
+
+  @ViewChild(CalendarComponent) myCalendar!: CalendarComponent;
 
   constructor() {}
 
+  previousMonth() {
+    this.myCalendar.slidePrev();
+  }
+
+  nextMonth() {
+    this.myCalendar.slideNext();
+  }
+  onViewTitleChange(title: string) {
+    this.viewTitle = title;
+  }
 }

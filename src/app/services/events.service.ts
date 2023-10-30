8fc8a25e8ae3d2fb886d41e5a225fae1f1bc7708
Oliver Krumbæk Perving <Ollikp@gmail.com>
2023-10-30 12:01:21 +0100

    merge with master

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventsService {
+  private events: Event[] = [];
+  private eventUpdate = new Subject<Event[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  async init() {}
+
+  getEvents() {
+    this.http
+      .get<{ message: string; events: Event[] }>(
+        'http://localhost:3000/api/events'
+      )
+      .pipe(
+        map((data) => {
+          return data.events.map((event) => {
+            return {
+              eventid: event.eventid,
+              title: event.title,
+              allDay: event.allDay,
+              startTime: new Date(event.startTime),
+              endTime: new Date(event.endTime),
+              category: event.category,
+              subject: event.subject,
+              description: event.description,
+            };
+          });
+        })
+      )
+      .subscribe((transformedBlog) => {
+        this.events = transformedBlog;
+        this.eventUpdate.next(this.events);
+      });
+  }
+
+  UpdateEventListner() {
+    return this.eventUpdate.asObservable();
+  }
+}
+
+export interface Event {
+  eventid: number;
+  title: string;
+  allDay: boolean;
+  startTime: Date;
+  endTime: Date;
+  category: string;
+  subject: string;
+  description: string;
 }

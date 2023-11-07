1572ecc3d0c0e19375d96f979306cf0600e009b4
Seb-pro <sebastiankristensen1@gmail.com>
2023-11-07 08:42:31 +0100

    production true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAVVgsznm0sRy6L2aZc82MpUZ3MzPJahLA',
+    authDomain: 'powerskema.firebaseapp.com',
+    projectId: 'powerskema',
+    storageBucket: 'powerskema.appspot.com',
+    messagingSenderId: '192500335594',
+    appId: '1:192500335594:web:48f68ee3bbb2c65e776893',
+  },
 };
